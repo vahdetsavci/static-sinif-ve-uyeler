01b837af1da89b15822602aa1f65b818cc275697
Vahdet Savci <savcivahdet@gmail.com>
2023-11-13 14:09:50 +0300

    app is done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,58 @@
-﻿namespace static_sinif_ve_uyeler;
+﻿using System;
+
+namespace static_sinif_ve_uyeler;
 
 class Program
 {
     static void Main(string[] args)
-    {}
+    {
+        Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+        Calisan calisan = new Calisan("Ayşe", "Yılmaz", "IK");
+        Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+        Calisan calisan1 = new Calisan("Derya", "Yıldırım", "IK"),
+                calisan2 = new Calisan("Vahdet", "Savcı", "IT");
+        Console.WriteLine("Çalışan Sayısı: {0}", Calisan.CalisanSayisi);
+
+        Console.WriteLine("Toplama işleminin sonucu: {0}", Islemler.Topla(100,200));
+        Console.WriteLine("Çıkarma işleminin sonucu: {0}", Islemler.Cikar(400,50));
+    }
+}
+
+class Calisan
+{
+    private static int calisanSayisi;
+
+    internal static int CalisanSayisi { get => calisanSayisi;}
+
+    private string isim;
+    private string soyisim;
+    private string departman;
+
+    static Calisan()
+    {
+        calisanSayisi = 0;
+    }
+
+    internal Calisan(string isim, string soyisim, string departman)
+    {
+        this.isim = isim;
+        this.soyisim = soyisim;
+        this.departman = departman;
+        calisanSayisi++;
+    }
+}
+
+static class Islemler
+{
+    internal static long Topla(int sayi1, int sayi2)
+    {
+        return sayi1 + sayi2;
+    }
+
+    internal static long Cikar(int sayi1, int sayi2)
+    {
+        return sayi1 - sayi2;
+    }
 }
